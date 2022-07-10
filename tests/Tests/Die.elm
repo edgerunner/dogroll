15f67aa5e6ae3a3274525baf176e69956f05792f
Mert Torun <mert@merttorun.com>
2022-07-10 15:33:07 +0300

    Brainstorm Die API

---
+++ tests/Tests/Die.elm
@@ -0,0 +1,14 @@
+module Tests.Die exposing (..)
+
+import Expect
+import Test exposing (Test, describe, test, todo)
+
+
+suite : Test
+suite =
+    describe "Die"
+        [ todo "initialize with number of sides and seed"
+        , todo "report number of sides"
+        , todo "roll"
+        , todo "report last roll"
+        ]
