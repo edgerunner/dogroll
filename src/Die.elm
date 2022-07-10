f9c95137de3cf183f766f662568624607a266ee1
Mert Torun <mert@merttorun.com>
2022-07-10 18:00:31 +0300

    Get started with elm-format

--- src/Die.elm
+++ src/Die.elm
@@ -2,6 +2,7 @@ module Die exposing (Die, d10, d4, d6, d8, lastRoll, roll, sides, toString)
 
 import Random exposing (Seed)
 
+
 type Die
     = Die Sides Seed Int
 
@@ -12,36 +13,51 @@ type Sides
     | D8
     | D10
 
-init sides_ = 
+
+init sides_ =
     Random.initialSeed >> Die sides_ >> (|>) 0
+
 
 d4 : Int -> Die
 d4 =
     init D4
 
+
 d6 : Int -> Die
 d6 =
     init D6
+
 
 d8 : Int -> Die
 d8 =
     init D8
 
+
 d10 : Int -> Die
 d10 =
     init D10
+
 
 sides : Die -> Int
 sides (Die d _ _) =
     sidesToInt d
 
+
 sidesToInt : Sides -> Int
 sidesToInt d =
     case d of
-        D4 -> 4
-        D6 -> 6
-        D8 -> 8
-        D10 -> 10
+        D4 ->
+            4
+
+        D6 ->
+            6
+
+        D8 ->
+            8
+
+        D10 ->
+            10
+
 
 roll : Die -> Die
 roll (Die sides_ seed_ _) =
@@ -49,13 +65,13 @@ roll (Die sides_ seed_ _) =
         |> Random.int 1
         |> Random.step
         |> (|>) seed_
-        |> (\(newRoll, newSeed) -> Die sides_ newSeed newRoll)
+        |> (\( newRoll, newSeed ) -> Die sides_ newSeed newRoll)
 
 
 lastRoll : Die -> Int
 lastRoll (Die _ _ roll_) =
     roll_
-    
+
 
 toString : Die -> String
 toString =
--- tests/Tests/Die.elm
+++ tests/Tests/Die.elm
@@ -1,20 +1,20 @@
 module Tests.Die exposing (..)
 
 import Die exposing (Die)
-
 import Expect
-import Test exposing (Test, describe, test, fuzz)
 import Fuzz
+import Test exposing (Test, describe, fuzz, test)
 
 
 dice : List (Int -> Die)
 dice =
-    [Die.d4, Die.d6, Die.d8, Die.d10]
+    [ Die.d4, Die.d6, Die.d8, Die.d10 ]
 
 
 multipleDice : Fuzz.Fuzzer Die
 multipleDice =
     Fuzz.map2 (|>) Fuzz.int (Fuzz.oneOf (List.map Fuzz.constant dice))
+
 
 suite : Test
 suite =
@@ -24,19 +24,20 @@ suite =
                 dice
                     |> List.map ((|>) 0)
                     |> List.map Die.sides
-                    |> Expect.equal [4, 6, 8, 10]
+                    |> Expect.equal [ 4, 6, 8, 10 ]
             )
-        , fuzz multipleDice "rolling"
-            ( Die.roll 
-                >> (\d -> (d, d))
+        , fuzz multipleDice
+            "rolling"
+            (Die.roll
+                >> (\d -> ( d, d ))
                 >> Tuple.mapBoth Die.sides Die.lastRoll
-                >> (\(sides, side) ->
-                    side
-                        |> Expect.all
-                            [ Expect.atLeast 1
-                            , Expect.atMost sides
-                            ] 
-                    )
+                >> (\( sides, side ) ->
+                        side
+                            |> Expect.all
+                                [ Expect.atLeast 1
+                                , Expect.atMost sides
+                                ]
+                   )
             )
         , test "string representation"
             (\() ->
