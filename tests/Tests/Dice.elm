a5eda425e873f87298812772898e4ef37744dedc
Mert Torun <mert@merttorun.com>
2022-07-11 08:24:14 +0300

    Brainstorm Dice API

---
+++ tests/Tests/Dice.elm
@@ -0,0 +1,13 @@
+module Tests.Dice exposing (..)
+
+import Test exposing (Test, describe, todo)
+
+
+suite : Test
+suite =
+    describe "Dice"
+        [ todo "initializing multiple different dice"
+        , todo "combining dice"
+        , todo "rolling all dice at once"
+        , todo "getting the best raise"
+        ]
